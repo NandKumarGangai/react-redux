8f9267d2cafa58890b668b7dbe39c2e3a5008dd6
nandkumargangai <nandkumargangai1@outlook.com>
2019-10-09 21:35:53 +0530

    Moved store configuration.

---
+++ src/configurations/appReducers.js
@@ -0,0 +1,9 @@
+import { items, videoToPlay, requestStatus } from "../reducers/reducer";
+
+const appReducers = {
+    items,
+    videoToPlay,
+    requestStatus,
+};
+
+export default appReducers;
